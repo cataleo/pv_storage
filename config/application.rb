44d385db968c74e64a2c5ae53206ea6dc043aa6d
cataleo <catalin@autouncle.com>
2020-02-12 16:42:03 +0200

    Add config

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# config.assets.initialize_on_precompile = false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,7 +10,7 @@ Bundler.require(*Rails.groups)
 module PVDataStorage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 6.0
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
